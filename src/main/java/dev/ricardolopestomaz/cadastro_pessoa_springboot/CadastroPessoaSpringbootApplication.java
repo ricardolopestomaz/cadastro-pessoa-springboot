3a88193b3abf9f1d1380c0ed903793afa2284cd6
ricardolopestomaz <ricardolptz42@gmail.com>
2025-10-08 10:14:31 -0300

    first commit

---
+++ src/main/java/dev/ricardolopestomaz/cadastro_pessoa_springboot/CadastroPessoaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package dev.ricardolopestomaz.cadastro_pessoa_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroPessoaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroPessoaSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ricardolopestomaz/cadastro_pessoa_springboot/CadastroPessoaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ricardolopestomaz.cadastro_pessoa_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroPessoaSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
